68bc37d598c847c6a72c06f1f19bc5c10506b79e
alansarluv <alan.sarluf@gmail.com>
2020-11-21 22:06:58 +0700

    Communication between electron app to mainWindow

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const electron = require('electron');
 const { app, BrowserWindow, ipcMain } = electron;
 const ffmpeg = require('fluent-ffmpeg'); 
 
+let mainWindow;
+
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true
     }
@@ -14,6 +16,9 @@ app.on('ready', () => {
 
 ipcMain.on('video:submit', (event, path) => {
   ffmpeg.ffprobe(path, (err, metadata) => {
-    console.log('Video duration is :', metadata.format.duration)
+    mainWindow.webContents.send(
+      'video:metadata',
+      metadata.format.duration
+    );
   })
 });
