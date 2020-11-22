0bbf06df9025e7c1021b32a2d851071af5d1df55
alansarluv <alan.sarluf@gmail.com>
2020-11-22 23:12:25 +0700

    Rename file index to main and setup toolbar menu

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const electron = require('electron');
-const { app, BrowserWindow, ipcMain } = electron;
+const { app, BrowserWindow, ipcMain, Menu } = electron;
 const ffmpeg = require('fluent-ffmpeg'); 
 
 let mainWindow;
+
+// menuTemplate is used for define all menu inside main navbar
+let menuTemplate = [];
+menuTemplate.push(
+  {
+    label: 'File',
+    submenu: [
+      { label: 'newTodo' },
+      {
+        label: 'Add a to do'
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q', // used for hot keys (shortcut)
+        click() {
+          app.quit();
+        }
+      }
+    ],
+  }
+)
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({ label: '' });
+}
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -10,10 +34,14 @@ app.on('ready', () => {
       nodeIntegration: true
     }
   });
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-  // mainWindow.loadURL('https://hr.talenta.co');
+  mainWindow.loadURL(`file://${__dirname}/main.html`);
+  // mainWindow.loadURL('https://hr.talenta.co'); we can reference url to any web as well
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
 
+// ipc for fetch video duration using IPC communcation
 ipcMain.on('video:submit', (event, path) => {
   ffmpeg.ffprobe(path, (err, metadata) => {
     mainWindow.webContents.send(
@@ -22,3 +50,4 @@ ipcMain.on('video:submit', (event, path) => {
     );
   })
 });
+
